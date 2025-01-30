490f36fa38ea56d8d57f67c529307ebe697c6c1e
neque <neque.primo@gmail.com>
2025-01-30 13:05:48 -0300

    Remove deprecated example and add new operators demonstration

---
+++ Basics/04-Operators.js
@@ -0,0 +1,31 @@
+//* Opreadores
+
+//? Aritméticos
+
+console.log(2 + 2); // Suma
+console.log(5 - 3); // Resta
+console.log(2 * 3); // Multiplicación
+console.log(10 / 2); // División
+console.log(5 % 2); // Módulo
+console.log(2 ** 3); // Potencia
+
+let a = 5;
+let b = 10;
+console.log(a + b); // Suma
+
+console.log(a++); // Incremento
+console.log(b--); // Decremento
+
+//? Operadores de asignación
+
+let myVariable = 10;
+myVariable += 5;
+console.log(myVariable); // 15
+
+myVariable -= 5; //Menos 5
+myVariable *= 2; //Por 2
+myVariable /= 2; //Entre 2
+myVariable %= 2; //Módulo 2
+myVariable **= 2; //Potencia 2
+
+//? Operadores de comparación
